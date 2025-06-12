c12b46ff9f582b38581730f58192d7dbe9f07fd6
Abhinav Srivatsa Kondapalli <121254293+SerDunk@users.noreply.github.com>
2025-06-12 21:02:39 +0530

    Shadow test

--- src/components/ui/accordion.jsx
+++ src/components/ui/accordion.jsx
@@ -14,7 +14,7 @@ function AccordionItem({ className, ...props }) {
   return (
     <AccordionPrimitive.Item
       data-slot="accordion-item"
-      className={cn("border-b last:border-b-0", className)}
+      className={cn("border-b last:border-b-0 rounded-xl", className)}
       {...props}
     />
   );
@@ -26,7 +26,7 @@ function AccordionTrigger({ className, children, ...props }) {
       <AccordionPrimitive.Trigger
         data-slot="accordion-trigger"
         className={cn(
-          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180 drop-shadow-xl",
+          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-xl py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180 drop-shadow-xl",
           className
         )}
         {...props}
@@ -42,7 +42,7 @@ function AccordionContent({ className, children, ...props }) {
   return (
     <AccordionPrimitive.Content
       data-slot="accordion-content"
-      className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm"
+      className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm rounded-xl"
       {...props}
     >
       <div className={cn("pt-0 pb-4", className)}>{children}</div>
