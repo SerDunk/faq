9ad2997487a2f819dfcb9c9e3f6ca11e7e8d0d71
Abhinav Srivatsa Kondapalli <121254293+SerDunk@users.noreply.github.com>
2025-06-12 20:54:20 +0530

    Removed shadows as no match

--- src/app/page.js
+++ src/app/page.js
@@ -78,7 +78,7 @@ export default function FAQ() {
               <AccordionItem
                 key={faq.id}
                 value={faq.id}
-                className="rounded-xl mb-4 overflow-hidden border-none drop-shadow-md"
+                className="rounded-xl mb-4 overflow-hidden border-none"
                 style={{
                   background: "linear-gradient(to right, #E3E3E3, #FFFFFF)",
                 }}
