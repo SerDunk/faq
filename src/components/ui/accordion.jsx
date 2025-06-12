2d540195c5fa371c825a76c764af1287e228c658
Abhinav Srivatsa Kondapalli <121254293+SerDunk@users.noreply.github.com>
2025-06-12 21:09:33 +0530

    Removed shadows from .jsx file

--- src/components/ui/accordion.jsx
+++ src/components/ui/accordion.jsx
@@ -26,7 +26,7 @@ function AccordionTrigger({ className, children, ...props }) {
       <AccordionPrimitive.Trigger
         data-slot="accordion-trigger"
         className={cn(
-          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-xl py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180 drop-shadow-xl",
+          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-xl py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
           className
         )}
         {...props}
