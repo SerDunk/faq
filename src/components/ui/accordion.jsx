ba76e5d690f04742844ee4cb7e02eadee16a7cc5
Abhinav Srivatsa Kondapalli <121254293+SerDunk@users.noreply.github.com>
2025-06-12 21:07:35 +0530

    Dropped shadows

--- src/components/ui/accordion.jsx
+++ src/components/ui/accordion.jsx
@@ -14,7 +14,7 @@ function AccordionItem({ className, ...props }) {
   return (
     <AccordionPrimitive.Item
       data-slot="accordion-item"
-      className={cn("border-b last:border-b-0 rounded-xl", className)}
+      className={cn("border-b last:border-b-0 rounded-xl ", className)}
       {...props}
     />
   );
